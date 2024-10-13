f44ff149f83ddae287e8834da142985065f4ced2
Cromatin <lars-winzer@web.de>
2024-10-13 14:53:10 +0200

    Fixed docstring

--- src/utils/datetime_tools.py
+++ src/utils/datetime_tools.py
@@ -16,6 +16,6 @@ def get_elapsed_time_smal(timestamp:float) -> str:
     return f"{time_elapsed.second:02}sec {time_elapsed.microsecond // 1000:03}ms"
 
 def get_elapsed_time_big(timestamp:float) -> str:
-    """Convert an timestamp into an predefined elapsed time format (00sec 000ms)"""
+    """Convert an timestamp into an predefined elapsed time format (00days 00hrs 00min)"""
     time_elapsed = datetime.fromtimestamp(timestamp)
     return f"{time_elapsed.day - 1}days {time_elapsed.hour - 1}hrs {1 if time_elapsed.minute == 0 else time_elapsed.minute}min"
