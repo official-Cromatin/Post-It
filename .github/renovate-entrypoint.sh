13a90753763cf244847713bdd3f965d35bd47628
official-Cromatin <lars-winzer@web.de>
2025-09-21 17:26:08 +0200

    Modify renovate workflow to use docker-mount

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
 echo "=== Starting Renovate Bot ==="
+
+cd /github/workspace
 
 apt update
 apt install -y build-essential libpq-dev
 
-runuser -u ubuntu renovate --platform=github
+runuser -u ubuntu -- renovate --platform=github
 
 echo "=== Renovate Bot finished ==="
