official-Cromatin/Post-It

commit 79e335bf2c2f171f744686f4c08e494d5200c6f0

Update: Entry point to now use asyncio.run
As of >3.10 .get_event_loop() is deprecated

src/main.py
@@ -10,7 +10,7 @@ from bot import PostIt_Bot
 from utils.datetime_tools import get_elapsed_time_milliseconds, get_elapsed_time_big
 import discord
 
-async def main(event_loop:asyncio.AbstractEventLoop, close_event:asyncio.Event):
+async def main(event_loop:asyncio.AbstractEventLoop):
     PROGRAM_VERSION = "1.0"
     print("      ____  ____  ___________    __________")
     print("     / __ \/ __ \/ ___/_  __/   /  _/_  __/")
@@ -42,7 +42,6 @@ async def main(event_loop:asyncio.AbstractEventLoop, close_event:asyncio.Event):
     def signal_handler(*args):
         app_logger.info("Shutdown signal recieved, shutting down")
         event_loop.call_soon_threadsafe(shutdown_event.set)
-        close_event.set()
 
     match sys.platform:
         case "linux":
@@ -109,17 +108,8 @@ async def main(event_loop:asyncio.AbstractEventLoop, close_event:asyncio.Event):
         quit(1)
 
     finally:
-        close_event.set()
         app_logger.info(f"Exiting. Application ran for {get_elapsed_time_big(datetime.now().timestamp() - startup_time)}")
 
 # Entry point for execution
 if __name__ == "__main__":
-    event_loop = asyncio.get_event_loop()
-    asyncio.set_event_loop(event_loop)
-    close_event = asyncio.Event()
-
-    event_loop.run_until_complete(main(event_loop, close_event))
-
-    # Wait until program is ready to close
-    event_loop.run_until_complete(close_event.wait())
-    event_loop.close()
+    asyncio.run(main(asyncio.get_event_loop()))

commit fc9873402f1c4170f957e1356f91c7a2e92325fc

Fix: Startup of application after 79e335bf2c2f171f744686f4c08e494d5200c6f0 broke it

src/main.py
@@ -10,7 +10,7 @@ from bot import PostIt_Bot
 from utils.datetime_tools import get_elapsed_time_milliseconds, get_elapsed_time_big
 import discord
 
-async def main(event_loop:asyncio.AbstractEventLoop):
+async def main():
     PROGRAM_VERSION = "1.0"
     print("      ____  ____  ___________    __________")
     print("     / __ \/ __ \/ ___/_  __/   /  _/_  __/")
@@ -41,18 +41,18 @@ async def main(event_loop:asyncio.AbstractEventLoop):
     shutdown_event = asyncio.Event()
     def signal_handler(*args):
         app_logger.info("Shutdown signal recieved, shutting down")
-        event_loop.call_soon_threadsafe(shutdown_event.set)
+        shutdown_event.set()
 
     match sys.platform:
         case "linux":
             app_startup_logger.info("Detected platform: Linux")
-            event_loop.add_signal_handler(signal.SIGINT, signal_handler)
-            event_loop.add_signal_handler(signal.SIGTERM, signal_handler)
+            signal.signal(signal.SIGINT, signal_handler)
+            signal.signal(signal.SIGTERM, signal_handler)
 
         case "darwin":
             app_startup_logger.info("Detected platform: MacOS (Darwin)")
-            event_loop.add_signal_handler(signal.SIGINT, signal_handler)
-            event_loop.add_signal_handler(signal.SIGTERM, signal_handler)
+            signal.signal(signal.SIGINT, signal_handler)
+            signal.signal(signal.SIGTERM, signal_handler)
 
         case "win32":
             app_startup_logger.info("Detected platform: Windows (Win32)")
@@ -90,8 +90,8 @@ async def main(event_loop:asyncio.AbstractEventLoop):
     try:
         bot_instance = PostIt_Bot(startup_time, PROGRAM_VERSION, app_logger)
 
-        bot_task = event_loop.create_task(bot_instance.start(os.getenv("DISCORD_TOKEN")))
-        shutdown_task = event_loop.create_task(shutdown_event.wait())
+        bot_task = asyncio.create_task(bot_instance.start(os.getenv("DISCORD_TOKEN")))
+        shutdown_task = asyncio.create_task(shutdown_event.wait())
 
         # Wait if either the bot disconnects or the shutdown event is detected
         finished_task, _ = await asyncio.wait(
@@ -112,4 +112,4 @@ async def main(event_loop:asyncio.AbstractEventLoop):
 
 # Entry point for execution
 if __name__ == "__main__":
-    asyncio.run(main(asyncio.get_event_loop()))
+    asyncio.run(main())